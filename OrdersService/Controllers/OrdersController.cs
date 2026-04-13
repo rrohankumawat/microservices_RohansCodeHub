d6babe21b7ddab029f7f9065307b037188d49d23
Rohan kumawat <rohankumawat39@gmail.com>
2026-04-13 23:47:46 +0530

    Project upload

---
+++ OrdersService/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet("GetOrders")]
+        public IActionResult GetOrders()
+        {
+            var orders = new[]
+            {
+                new { Id = 1, Product = "Laptop", Quantity = 2 },
+                new { Id = 2, Product = "Smartphone", Quantity = 5 },
+                new { Id = 3, Product = "Headphones", Quantity = 10 }
+            };
+            return Ok(orders);
+        }
+    }
+}
---
+++ ProductsServices/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new[]
+            {
+                new { Id = 1, Name = "Laptop", Price = 999.99 },
+                new { Id = 2, Name = "Smartphone", Price = 499.99 },
+                new { Id = 3, Name = "Headphones", Price = 199.99 }
+            };
+            return Ok(products);
+        }
+    }
+}
